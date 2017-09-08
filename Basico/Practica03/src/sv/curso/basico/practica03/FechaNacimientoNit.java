047e906c266d101e65f9810e42524910e09935e0
Hector Tino <htino@multisistemas.com.sv>
2017-09-07 23:55:06 -0600

    Practica realizada dia 3

--- Basico/Practica03/src/sv/curso/basico/practica03/FechaNacimientoNit.java
+++ Basico/Practica03/src/sv/curso/basico/practica03/FechaNacimientoNit.java
@@ -5,10 +5,30 @@
  */
 package sv.curso.basico.practica03;
 
+import javax.swing.JOptionPane;
 /**
  *
  * @author Hector Tino
  */
 public class FechaNacimientoNit {
+    public static void main(String[] args) {
+        String numeroNit;
+        numeroNit = JOptionPane.showInputDialog("Ingrese su Numero de NIT:");
+        String resultado = FechaNacimientoNit.fechaNacimiento(numeroNit);
+        JOptionPane.showMessageDialog(null, "Su fecha de nacimento es: " + resultado);
+    }
     
+    public static String fechaNacimiento(String nit){
+        //0210-080483-101-2
+        //0123 456789 112 3
+        String separador = " / ";
+        String diaNac = nit.substring(4, 6);
+        String mesNac = nit.substring(6, 8);
+        String anoNac = nit.substring(8, 10);
+        
+        String patron = diaNac + separador + mesNac + separador + anoNac;
+        //String fechaNac = String.format(patron, separador);
+        
+        return patron;
+    }
 }
